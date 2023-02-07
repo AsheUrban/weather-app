902182be5d4d4c2206906467fd8245e6adb67593
Ashe Urban <ashe@goldentongue.com>
2023-02-07 10:50:36 -0800

    Finish through lesson 18.
    Co-authored-by: John <john.lenz27@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>

--- src/index.js
+++ src/index.js
@@ -5,33 +5,39 @@ import "./css/styles.css";
 
 // Business Logic
 
-function getWeather (city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-  
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+function getWeather(city) {
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    request.addEventListener("loadend", function() {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, city]);
+      } else {
+        reject([this, response, city]);
+      }
   });
 
   request.open("GET", url, true);
   request.send();
-}
+});
 
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
+}
 
 // UI Logic
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText =  `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printElements(results) {
+  document.querySelector('#showResponse').innerText = `The Humitdity in ${results[1]} is ${results[0].main.humidity}%.
+  The temperature in Kelvins is ${results[0].main.temp} degrees.`;
 }
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The Humitdity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+function printError(error) {
+  document.querySelector('#showResponse').innerText =  `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
 
 function handleFormSubmission(event) {

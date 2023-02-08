7ccdc1f512b053c1b7a1dc85b3dbb6bc018abf17
Ashe Urban <ashe@goldentongue.com>
2023-02-08 11:55:21 -0800

    Finish refactoring async-await branch.

--- src/index.js
+++ src/index.js
@@ -15,24 +15,28 @@ async function getWeather(city) {
 
 // UI Logic
 
-function printElements(results) {
-  document.querySelector('#showResponse').innerText = `The Humitdity in ${results[1]} is ${results[0].main.humidity}%.
-  The temperature in Kelvins is ${results[0].main.temp} degrees.`;
+function printElements(response, city) {
+  document.querySelector(
+    "#showResponse"
+  ).innerText = `The Humitdity in ${city} is ${response.main.humidity}%.
+  The temperature in Kelvins is ${response.main.temp} degrees.`;
 }
 
-function printError(error) {
-  document.querySelector('#showResponse').innerText =  `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+function printError(error, city) {
+  document.querySelector(
+    "#showResponse"
+  ).innerText = `There was an error accessing the weather data for ${city}: ${error}.`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
+  const city = document.querySelector("#location").value;
+  document.querySelector("#location").value = null;
   getWeather(city);
 }
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+window.addEventListener("load", function () {
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleFormSubmission);
 });
-
-  
